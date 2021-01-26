ca08a01734fe05339ab8fd10e2ac9a3bd5f700f6
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-01-26 13:16:31 +0530

    Added tasks component using global state

--- src/components/Button.js
+++ src/components/Button.js
@@ -13,7 +13,8 @@ Button.defaultProps = {
 
 Button.propTypes = {
     text: PropTypes.string,
-    color: PropTypes.string
+    color: PropTypes.string,
+    onClick: PropTypes.func
 }
 
 export default Button
